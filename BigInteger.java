a748b3a61eda7d58d2376b494c7af58feefa9500
Aakash7khadka <35725916+Aakash7khadka@users.noreply.github.com>
2019-07-30 18:50:56 +0545

    Create BigInteger.java

---
+++ BigInteger.java
@@ -0,0 +1,14 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class hack
+{
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		BigInteger b1=new BigInteger(scan.next());
+		BigInteger b2=new BigInteger(scan.next());
+		System.out.println(b1.multiply(b2));
+		
+		
+	}
+}
